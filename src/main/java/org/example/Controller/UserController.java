62ee770549e895e32cd7b1dbe5294ef3395a2e38
Nurda_Cyber <amirnurdaulet668@gmail.com>
2024-12-07 14:18:03 +0500

    Өзгерістер енгізілді: Home бетке қайту батырмасы қосылды

--- src/main/java/org/example/Controller/UserController.java
+++ src/main/java/org/example/Controller/UserController.java
@@ -1,7 +1,29 @@
 package org.example.Controller;
 
-import jdk.jfr.Registered;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
+@Controller
 public class UserController {
+
+    @GetMapping("/Login")
+    public String User(){
+
+        return "Login";
+    }
+
+    @GetMapping("/Registration")
+    public String Registration(){
+
+        return "Registration";
+    }
+    @GetMapping("/Home")
+    public String Home(){
+
+        return "Home";
+    }
 }
